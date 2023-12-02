8ea0258da27f3a9e7b41bd69c9df6f6f64658848
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-02 15:07:39 +0300

    all tests passed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1,"Mr. Chocolate", 15, "M", "black", "aggressive", 1),
+(2,"Rowdy", 13, "M", "brown", "gentle", 1),
+(3,"Tabitha", 13, "F", "brown", "playful", 0),
+(4,"Sergeant Brown", 18, "M", "blue", "calm", 0),
+(5,"Melissa", 9, "F", "black", "energetic", 1),
+(6,"Grinch", 7, "M", "brown", "friendly", 1),
+(7,"Wendy", 9, "F", "blonde", "shy", 1),
+(8,null, 11, "F", "cinnamon", "mischievous", 0);
